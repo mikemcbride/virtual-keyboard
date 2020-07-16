9b3868f1b32ec88fe68277c05f7cb294051797a1
Mike McBride <mike.mcbride@hey.com>
2020-07-16 13:55:49 -0500

    add an api for pageviews

--- www/src/App.vue
+++ www/src/App.vue
@@ -13,6 +13,14 @@ export default {
   name: 'App',
   components: {
     Keyboard
-  }
+  },
+  mounted() {
+    const { timeZone, locale } = Intl.DateTimeFormat().resolvedOptions()
+    fetch('/api/pageview', {
+      method: 'POST',
+      body: JSON.stringify({ timeZone, locale }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+  },
 }
 </script>
